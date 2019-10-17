77c945f36d8ac09c594db77aed7544cad96a2759
RaissaMariaB <raissamaria.sb@gmail.com>
2019-10-17 16:22:14 -0300

    adiciona exercício 1 e 2 de jquery ajax

--- 3 - Efeitos/Exercícios/site/js/page3.js
+++ 3 - Efeitos/Exercícios/site/js/page3.js
@@ -1,9 +1,34 @@
-$(function(){
+$(function () {
+    
+    function configurateHover() {
+        $(".card-title").off("mouseEnter").on("mouseenter",
+            function () {
+                $(this).css("color", "yellow")
+            }
+        )
 
-    $(".btn-primary").click(function(){
+        $(".card-title").off("mouseLeave").on("mouseleave",
+            function () {
+                $(this).css("color", "black")
+            })
+
+    }
+
+    configurateHover()
+    
+    $(".btntoggle").click(function () {
         $(".card1").fadeToggle(3000)
         $(".card2").fadeToggle(5000)
         $(".card3").fadeToggle(7000)
+    })
+    
+    $(".btncopia").click(function () {
+        let copia = $(".div-origin").html()
+
+        $(".div-origin").after('<div class="row">' + copia + "</div>")
+
+        configurateHover()
+
     })
 
 })
